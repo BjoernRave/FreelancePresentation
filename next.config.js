c28633b132adc65b1ab8fb68598f44914a4d7257
bjoernRave <bjoern.rave@gmail.com>
2020-02-05 16:08:47 +0000

    add first stuff

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 const withOptimizedImages = require("next-optimized-images");
+const withCSS = require("@zeit/next-css");
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true"
 });
 
-module.exports = withOptimizedImages(withBundleAnalyzer());
+module.exports = withCSS(withOptimizedImages(withBundleAnalyzer()));
